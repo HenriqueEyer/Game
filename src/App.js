937049a24f635b8b0469c76a373383794159407a
HenriqueEyer <henriqueeyer@outlook.com>
2021-09-18 11:56:46 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
+const socket = io('http://localhost:4555', { transports : ['websocket'] });
+socket.on('connection', () => console.log('LOGOU'))
+
+const initialState = {
+  username: '',
+  login: false,
+}
 
 function App() {
+  const [user, setUser] = useState(initialState);
+  const [number, setNumber] = useState('');
+
+  const logon = () => {
+    socket.emit('login', user );
+  }
+
+  const username = (e) => {
+    setUser({...user, username: e.target.value});
+  }
+
+  socket.on('success-login', () => setUser({...user, login:true }));
+  socket.on('get-number', (value) => setNumber(value.number));
+
+  useEffect(() => {
+    console.log(number)
+  },[number])
+
+  useEffect(() => {
+    console.log(user)
+  },[user])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Realize seu login:</p>
+      <input type="text" onChange={username}/>
+      <input type="button" onClick={()=> logon()} value="Login" />
     </div>
   );
 }
